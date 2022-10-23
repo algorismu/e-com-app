6f20e461d8386396cadb42f563b9867b71536b94
Muhammad Eid <algorismu.me@gmail.com>
2022-10-23 16:57:55 +0200

    Prototype the product list component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/Product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  items: Product[] = [];
 
-  constructor() { }
+  constructor(private products: ProductService) {}
 
   ngOnInit(): void {
+    this.products.findAll().subscribe((foundProducts) => {
+      this.items = foundProducts;
+    });
   }
-
 }
