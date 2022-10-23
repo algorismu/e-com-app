e532df43f88031090cb68237a156f5ea8877b240
Muhammad Eid <algorismu.me@gmail.com>
2022-10-23 11:08:59 +0200

    Implement product service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from './models/Product';
+import { ProductListMock } from './models/Product-List.mock';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private products: Product[] = ProductListMock;
 
-  constructor() { }
+  findAll(): Observable<Product[]> {
+    return of(this.products);
+  }
 }
