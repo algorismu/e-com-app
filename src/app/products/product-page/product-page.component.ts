4317ed6c99ca6ba83703418ce2b3e3ceb87019aa
Muhammad Eid <algorismu.me@gmail.com>
2022-10-23 16:58:55 +0200

    Prototype the product page component

--- src/app/products/product-page/product-page.component.ts
+++ src/app/products/product-page/product-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/Product';
+import { productList } from '../models/Product-List.mock';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  styleUrls: ['./product-page.component.css'],
 })
 export class ProductPageComponent implements OnInit {
-
-  constructor() { }
+  product!: Product;
 
   ngOnInit(): void {
+    this.product = productList[0];
   }
 
+  addToCart(): void {}
 }
