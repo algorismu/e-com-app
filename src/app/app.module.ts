8a5aa1c451b7e8cec5410ca7c72cbbf379df4047
Muhammad Eid <algorismu.me@gmail.com>
2022-10-27 12:09:10 +0200

    Import cart module into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartModule } from './cart/cart.module';
 import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ProductsModule],
+  imports: [BrowserModule, AppRoutingModule, ProductsModule, CartModule],
   providers: [],
   bootstrap: [AppComponent],
 })
