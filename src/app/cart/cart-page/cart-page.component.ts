2514d90e239f6b9d5b15aeca55fcf0bc726395dc
Muhammad Eid <algorismu.me@gmail.com>
2022-10-27 12:06:20 +0200

    Prototype cart page component

---
+++ src/app/cart/cart-page/cart-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css'],
+})
+export class CartPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
