3459807be2c97f704659368b329ecb96eff25267
Muhammad Eid <algorismu.me@gmail.com>
2022-10-27 12:05:53 +0200

    Prototype cart component

---
+++ src/app/cart/cart-list/cart.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { CartItem } from '../models/CartItem.model';
+
+@Component({
+  selector: 'cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit {
+  items!: CartItem[];
+  subTotal!: number;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.fillCart();
+    this.updateSubTotal();
+  }
+
+  updateCart(item: CartItem): void {
+    this.cartService.add(item);
+    this.fillCart();
+    this.updateSubTotal();
+  }
+
+  private fillCart(): void {
+    this.cartService.findAll().subscribe((cartItems) => {
+      this.items = cartItems;
+    });
+  }
+
+  private updateSubTotal(): void {
+    this.cartService
+      .calcSubTotalPrice()
+      .subscribe((value) => (this.subTotal = value));
+  }
+}
